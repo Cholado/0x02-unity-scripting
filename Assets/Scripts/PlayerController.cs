4c83fa68209705de6f7f2173eb95511f987cf0f9
Santiago Efrain Martinez Leon <60367880+Cholado@users.noreply.github.com>
2020-12-25 11:22:38 -0500

    maze PogU

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 1000f;
+    public Rigidbody rb;
+    private int score = 0;
+    public int health = 5;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Update once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene("maze");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score += 1;
+            Debug.Log($"Score: {score}");
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.tag == "Trap")
+        {
+            health -= 1;
+            Debug.Log($"Health: {health}");
+        }
+
+        if (other.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
+    }
+}
